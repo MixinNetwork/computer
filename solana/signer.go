67ac4587ce21718504d1d27610cbaaebd0b1f19e
hundredark <hundredark@gmail.com>
2025-08-13 21:13:50 +0800

    fail request when cannot find call

--- solana/signer.go
+++ solana/signer.go
@@ -743,10 +743,10 @@ func (node *Node) processSignerSignatureResponse(ctx context.Context, req *store
 		panic(fmt.Errorf("store.ReadSession(%s) => %v %v", sid, s, err))
 	}
 	call, err := node.store.ReadSystemCallByRequestId(ctx, s.RequestId, common.RequestStatePending)
-	if err != nil || call == nil {
-		panic(fmt.Errorf("store.ReadSystemCallByRequestId(%s) => %v %v", s.RequestId, call, err))
+	if err != nil {
+		panic(fmt.Errorf("store.ReadSystemCallByRequestId(%s) => %v", s.RequestId, err))
 	}
-	if call.State == common.RequestStateFailed || call.Signature.Valid {
+	if call == nil || call.State == common.RequestStateFailed || call.Signature.Valid {
 		logger.Printf("invalid call %s: %d %s", call.RequestId, call.State, call.Signature.String)
 		return node.failRequest(ctx, req, "")
 	}
